b84e1f69b6456f097405b06faae108df450074b0
Ryan Johnson <rjohnson@strenuus.com>
2019-11-13 20:12:02 -0600

    Adding unit test

---
+++ Tests/ParsedObjectConvertTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ParsedObjectConvertTests
+    {
+        [TestCase(FakeData.FakeRawRequests.GetWithoutQueryString)]
+        [TestCase(FakeData.FakeRawRequests.PostWithRequestBody)]
+        public void Should_Convert_ParsedRequest_Back_To_String(string input)
+        {
+            var parsed = HttpParser.Parser.ParseRawRequest(input);
+
+            Assert.AreEqual(input, parsed.ToString());
+        }
+    }
+}
